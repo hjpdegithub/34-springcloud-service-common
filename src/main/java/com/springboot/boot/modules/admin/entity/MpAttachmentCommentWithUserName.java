551b8f4f68be7b54224e5344877abbd0dd2c16c8
jianpeng.hou@qq.com <jianpeng.hou@qq.com>
2022-08-02 15:38:55 +0800

    112

---
+++ src/main/java/com/springboot/boot/modules/admin/entity/MpAttachmentCommentWithUserName.java
@@ -0,0 +1,115 @@
+package com.springboot.boot.modules.admin.entity;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import java.util.Date;
+
+public class MpAttachmentCommentWithUserName {
+    @JsonSerialize(using = ToStringSerializer.class)//解决long精度丢失问题
+    private Long id;
+
+    private String userName;
+
+    @JsonSerialize(using = ToStringSerializer.class)//解决long精度丢失问题
+    private Long curId;
+
+    @JsonSerialize(using = ToStringSerializer.class)//解决long精度丢失问题
+    private Long userId;
+
+    private String comment;
+
+    private String description;
+
+    private Integer delFlag;
+    @JsonSerialize(using = ToStringSerializer.class)//解决long精度丢失问题
+
+    private Long createUser;
+
+    private Date createDate;
+
+    @JsonSerialize(using = ToStringSerializer.class)//解决long精度丢失问题
+    private Long updateUser;
+
+    private Date updateDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCurId() {
+        return curId;
+    }
+
+    public void setCurId(Long curId) {
+        this.curId = curId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment == null ? null : comment.trim();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public Integer getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(Integer delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public Long getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Long createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Long getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Long updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+}
